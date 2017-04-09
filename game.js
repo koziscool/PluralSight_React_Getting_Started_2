ad74c37fe8356f0c02f676115c285ceff3e06b87
koziscool <kozjob@yahoo.com>
2017-04-09 15:29:41 -0500

    more selecting ui

--- game.js
+++ game.js
@@ -2,12 +2,9 @@
 
 var Stars = React.createClass({
 
-
   render: function() {
-    
-    const numberOfStars = 1+ Math.floor( Math.random() * 9 );
     let stars = [];
-    for (let i = 0; i< numberOfStars; i++) {
+    for (let i = 0; i< this.props.numberOfStars; i++) {
       stars.push(<i key={i} className="fa fa-star"></i> )
     }
     return (
@@ -33,7 +30,9 @@ var Answer = React.createClass({
     return (
       <div className="col-5">
         { this.props.selectedNumbers.map( (number) => 
-          <span key={number}>{number}</span>) }
+          <span key={number} onClick={() => this.props.unselectNumber(number) }>
+            {number}
+          </span>) }
       </div>
     );
   }
@@ -50,7 +49,10 @@ var Numbers = React.createClass({
       <div className="card text-center">
         <div>
           {Numbers.list.map((number) => 
-            <span key={number} className={this.numberClassName(number) }>{number}</span>)}
+            <span key={number} className={this.numberClassName(number) }
+                                              onClick={ () => this.props.selectNumber(number)}>
+              {number}
+            </span>)}
         </div>
       </div>
     );
@@ -61,7 +63,23 @@ Numbers.list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var Game = React.createClass({
   getInitialState:function(){
-    return {selectedNumbers: [2, 4] }
+    return {
+      selectedNumbers: [],
+      numberOfStars: 1 + Math.floor( Math.random() * 9 ) 
+    }
+  },
+
+  selectNumber: function( clickedNumber ) {
+    if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0 ) { return; }
+    this.setState({
+      selectedNumbers: this.state.selectedNumbers.concat(clickedNumber)
+    })
+  },
+
+  unselectNumber: function( clickedNumber ) {
+    this.setState({
+      selectedNumbers: this.state.selectedNumbers.filter( (number) => number !== clickedNumber )
+    })
   },
 
   render: function() {
@@ -69,12 +87,14 @@ var Game = React.createClass({
       <div className="container">
         <h3>Play Nine</h3>
         <div className="row">
-          <Stars />
+          <Stars numberOfStars={this.state.numberOfStars}/>
           <Button />
-          <Answer selectedNumbers={ this.state.selectedNumbers }/>
+          <Answer selectedNumbers={ this.state.selectedNumbers }
+                          unselectNumber={this.unselectNumber} />
         </div>
         <br />
-        <Numbers selectedNumbers={ this.state.selectedNumbers }/>
+        <Numbers selectedNumbers={ this.state.selectedNumbers }
+                          selectNumber={this.selectNumber} />
       </div>
     );
   }
